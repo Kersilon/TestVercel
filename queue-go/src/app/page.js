1d55955228c878a0eea23b5678015011eaaf2e44
RobertoAM1 <robertoandrei.miron@gmail.com>
2023-06-17 16:56:36 +0200

    Test Merge 2

--- queue-go/src/app/page.js
+++ queue-go/src/app/page.js
@@ -105,7 +105,7 @@ export default function Home() {
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
-            123
+            321
           </p>
         </a>
       </div>
