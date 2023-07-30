58bc75b0e1b3507c96bed5617bbc034191c3c5ca
RobertoAM1 <robertoandrei.miron@gmail.com>
2023-07-30 20:17:37 +0200

    Update Personal area features

--- queue-go/next.config.js
+++ queue-go/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains:["images.pexels.com","img.freepik.com","icon-library.com","icon-library.com","static.vecteezy.com2","i.imgur.com"],
+        domains:["images.pexels.com","img.freepik.com","icon-library.com","icon-library.com","static.vecteezy.com2","i.imgur.com","m.media-amazon.com"],
     },
 }
 
--- queue-go/src/app/personalArea/page.jsx
+++ queue-go/src/app/personalArea/page.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from "./personalArea.module.css"
+import Link from 'next/link'
 
 const personalArea= () => {
   return (
@@ -8,33 +9,19 @@ const personalArea= () => {
           <h1>Area Personale</h1>
         </div>
         <div className={styles.containerFeature}>
+        <Link href="/">
           <div class={styles.feature}>
-            <a target="_blank" href="/">
-              <img src="/Queue&GoLogoS.png" alt="Cinque Terre"/>
-            </a>
-            <div class="desc">feature</div>
+              <img src="https://m.media-amazon.com/images/G/29/x-locale/cs/help/images/gateway/self-service/security._CB657833960_.png"/>
+            <div class={styles.featureText}>Cambio Password</div>
           </div>
+        </Link>  
 
+        <Link href="/">
           <div class={styles.feature}>
-            <a target="_blank" href="/">
-              <img src="/Queue&GoLogoS.png" alt="Cinque Terre"/>
-            </a>
-            <div class="desc">feature</div>
+              <img src="https://m.media-amazon.com/images/G/29/x-locale/cs/help/images/gateway/self-service/YA_icon_address_01._CB657833960_.png"/>
+            <div class={styles.featureText} >Cambio Informazioni Personali</div>
           </div>
-
-          <div class={styles.feature}>
-            <a target="_blank" href="/">
-              <img src="/Queue&GoLogoS.png" alt="Cinque Terre"/>
-            </a>
-            <div class="desc">feature</div>
-          </div>
-
-          <div class={styles.feature}>
-            <a target="_blank" href="/">
-              <img src="/Queue&GoLogoS.png" alt="Cinque Terre"/>
-            </a>
-            <div class="desc">feature</div>
-          </div>
+        </Link>  
         </div>
     </div>
   )
