7a3513b556f87f2d04d36ecd1e8704e244254768
Kersilon <ersiliopet@gmail.com>
2023-07-31 20:06:44 +0200

    Update QueueStatus
    piccola modifica al popUp:
    -cambiato l'id

--- queue-go/src/app/QueueStatus/page.jsx
+++ queue-go/src/app/QueueStatus/page.jsx
@@ -58,7 +58,7 @@ const queue = () => {
           <h1 className={styles.info}>tempo di attesa medio</h1>
         </div>
         <button className="button">Generate QR code</button>
-        <div class={styles.popupContainer} id="myPopup" onClick="placeholder">
+        <div class={styles.popupContainer} id="qrCodeContainer" onClick="placeholder">
           <img class={styles.popupImage} src="https://i.redd.it/r72kvnykeui41.png" alt="Qr code" />
           <h2 class={styles.popupClose}>X</h2>
         </div>
