59c2f76c77f4d544fc9d55bac1f8036784da4467
Kersilon <ersiliopet@gmail.com>
2023-08-01 21:54:52 +0200

    Update Queue Status
    -Adesso i popUp si possono anche chiudere

--- queue-go/src/app/QueueStatus/page.jsx
+++ queue-go/src/app/QueueStatus/page.jsx
@@ -59,6 +59,10 @@ const queue = () => {
     visibility: "hidden"
   });
 
+  const [qrCodeIsVisible, setQrCodeIsVisible] = useState({
+    visibility: "hidden"
+  })
+
   var account = JSON.parse(localStorage.getItem('account'));
   if(account != null && account.tipo == 'azienda') {
   return (
@@ -68,22 +72,28 @@ const queue = () => {
           <h1 className={styles.info}>x persone in fila</h1>
           <h1 className={styles.info}>tempo di attesa medio</h1>
         </div>
-        <button className="button">Generate QR code</button>
-        <div className={"popupContainer" + " " + styles.qrPopupContainer} id="qrCodeContainer" onClick="placeholder">
+        <button onClick={() => {setQrCodeIsVisible({visibility:"visible"});}} className="button">Generate QR code</button>
+        <div style={{visibility:`${qrCodeIsVisible.visibility}`}} className={"popupContainer" + " " + styles.qrPopupContainer} id="qrCodeContainer">
           <img class={styles.qrPopupImage} src="https://i.redd.it/r72kvnykeui41.png" alt="Qr code" />
-          <h2 class={styles.popupClose}>X</h2>
+          <h2 onClick={() => {setQrCodeIsVisible({visibility:"hidden"});}} class={styles.popupClose}>X</h2>
         </div>
       </div>
       <div className={styles.items}>
         {
           users.map(
             (user) => (
-              <Link href={path+user.name+" "+user.surname} className={styles.item}>
-              <span className={styles.title}>{user.id} {user.name} {user.surname}</span>
+              <Link href={""} className={styles.item}>
+              <div onClick={() => {setUserData({id: user.id, name: user.name, surname: user.surname, visibility:"visible"});}} className={styles.title}>{user.id} {user.name} {user.surname}</div>
               </Link>
             )
           )
         }
+      </div>
+      <div style={{visibility:`${userData.visibility}`}} className={"popupContainer" + " " + styles.userPopupContainer} id="userPopupContainer">
+          <h1>{userData.id}</h1>
+          <h1>{userData.name}</h1>
+          <h1>{userData.surname}</h1>
+          <h2 onClick={() => {setUserData({visibility:"hidden"});}} class={styles.popupClose}>X</h2>
       </div>
     </div>
   )
@@ -107,7 +117,7 @@ const queue = () => {
           )
         }
       </div>
-      <div style={{visibility:`${userData.visibility}`}} className={"popupContainer" + " " + styles.userPopupContainer} id="userPopupContainer" onClick="placeholder">
+      <div style={{visibility:`${userData.visibility}`}} className={"popupContainer" + " " + styles.userPopupContainer} id="userPopupContainer">
           <h1>{userData.id}</h1>
           <h1>{userData.name}</h1>
           <h1>{userData.surname}</h1>
