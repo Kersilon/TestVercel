a4749822bad09c06733c0948e1c5c5ab7eba2843
Kersilon <ersiliopet@gmail.com>
2023-08-03 14:44:50 +0200

    Update paginaMobile
    Cambiate alcune scritte

--- queue-go/src/app/paginaMobile/page.jsx
+++ queue-go/src/app/paginaMobile/page.jsx
@@ -26,7 +26,7 @@ const paginaMobile= () => {
   return (
     <div>
         <div class={styles.centeredTitle}>
-          <h2>Entra nella fila nei seguenti modi</h2>
+          <h2>Join a queue</h2>
         </div>
 
         <div class={styles.centeredContainer}>
@@ -34,14 +34,14 @@ const paginaMobile= () => {
           
           <div class={styles.centeredFunctions}>
           <Link href="/ricercaServizi"><img class={styles.imag} src="https://cdn3.iconfinder.com/data/icons/social-messaging-ui-color-line/245532/14-512.png"/></Link>  
-            <div >Ricerca servizi
+            <div >Search queue
             </div>
           </div>
         
        
           <div class={styles.centeredFunctions}>
           <Link href=""> <img onClick={openPopup} class={styles.imag} src="https://www.bew-web-agency.fr/wp-content/uploads/2022/03/qr-code-g494f94760_1280.png"/> </Link>
-            <div >Scansiona QR</div>
+            <div >QR code</div>
             {isClosed && qrCodePopup}
           </div>
           </div>
