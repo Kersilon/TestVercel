143345cebffebe9306d6eada718141cd3c3d503b
RobertoAM1 <robertoandrei.miron@gmail.com>
2023-08-07 15:41:37 +0200

    Bug fix

--- queue-go/src/components/navbar/Navbar.jsx
+++ queue-go/src/components/navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import styles from "./navbar.module.css"
 import Logo from "/public/Queue&GoLogoS.png";
 import Image from 'next/image'
 import { mockData } from "@/components/mockData";
+import { useRouter } from 'next/navigation'
 
 const linksAzienda = [
   {
@@ -40,12 +41,13 @@ const linksUnlogged = [
 
 
 const Navbar = () => {
+  const router = useRouter();
   var account = JSON.parse(localStorage.getItem('account'));
   var links = linksUnlogged;
   var button = "";
   if (account != null && account.tipo == 'azienda'){
     links = linksAzienda;
-    var button = <> <Link key='11' href="/QueueStatus/1" idSceltoclassName={styles.link}> Stato della fila </Link> <button className='button'onClick={() => { localStorage.removeItem('account'); location.reload();}} >Logout</button></>
+    var button = <> <Link key='11' href="/QueueStatus/1" idSceltoclassName={styles.link}> Stato della fila </Link> <button className='button' onClick={() => { localStorage.removeItem('account'); router.push('/')}} >Logout</button></>
   
   }else if(account !=null && account.tipo == 'utente'){
     var button = <> <Link key='10' href="/paginaMobile" idSceltoclassName={styles.link}> Unisciti ad una fila </Link> <button className='button'onClick={() => { localStorage.removeItem('account'); localStorage.removeItem('joinQueue'); location.reload();}} >Logout</button></> 
